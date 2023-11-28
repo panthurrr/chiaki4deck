1869ae983f19eac9417516a25944a247adf1e96e
Street Pea <streetpea@proton.me>
2023-11-28 00:18:04 -0800

    Update appimage for libplacebo

--- scripts/build-appimage.sh
+++ scripts/build-appimage.sh
@@ -8,17 +8,20 @@ appdir=${1:-`pwd`/appimage/appdir}
 mkdir appimage
 
 pip3 install --user protobuf==3.19.5
+rm -rf /usr/bin/meson
+pip3 install meson==1.3.0
 scripts/fetch-protoc.sh appimage
 export PATH="`pwd`/appimage/protoc/bin:$PATH"
 scripts/build-ffmpeg.sh appimage
 scripts/build-sdl2.sh appimage
+scripts/build-libplacebo.sh appimage
 
 mkdir build_appimage
 cd build_appimage 
 cmake \
 	-GNinja \
 	-DCMAKE_BUILD_TYPE=Release \
-	"-DCMAKE_PREFIX_PATH=`pwd`/../appimage/ffmpeg-prefix;`pwd`/../appimage/sdl2-prefix;/opt/qt512" \
+	"-DCMAKE_PREFIX_PATH=`pwd`/../appimage/ffmpeg-prefix;`pwd`/../appimage/sdl2-prefix;/opt/qt515" \
 	-DCHIAKI_ENABLE_TESTS=ON \
 	-DCHIAKI_ENABLE_GUI=ON \
 	-DCHIAKI_GUI_ENABLE_SDL_GAMECONTROLLER=ON \
@@ -43,7 +46,7 @@ set +e
 source /opt/qt515/bin/qt515-env.sh
 set -e
 
-export LD_LIBRARY_PATH="`pwd`/sdl2-prefix/lib:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="`pwd`/sdl2-prefix/lib:`pwd`/libplacebo/build/src:$LD_LIBRARY_PATH"
 export EXTRA_QT_PLUGINS=opengl
 
 ./linuxdeploy-x86_64.AppImage --appdir="${appdir}" -e "${appdir}/usr/bin/chiaki" -d "${appdir}/usr/share/applications/chiaki4deck.desktop" --plugin qt --output appimage
